8b5f5df629b7a36b1ee87a2ff551c06d4da52460
vic3king <vic3coorp@gmail.com>
2018-11-20 11:11:27 +0000

    [finishes #162050305] User can edit and save a red-flag

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
 let createRedTag = document.getElementById('btn1')
 let interventionRequest = document.getElementById('btn2')
 
+//event listener for redflag button
 createRedTag.addEventListener('click', (e) => {
   //effect to make sure only one form is being displayed at a time
   document.querySelector(".intervention").style.display = "none";
@@ -15,6 +16,7 @@ createRedTag.addEventListener('click', (e) => {
   e.preventDefault()
 })
 
+//event listener for intervention button
 interventionRequest.addEventListener('click', (e) => {
   //effect to make sure only one form is being displayed at a time
   document.querySelector(".red-flag").style.display = "none";
@@ -22,8 +24,41 @@ interventionRequest.addEventListener('click', (e) => {
   //create div and add a class
   let div = document.createElement("div")
   div.className = 'box'
-  
+
   //show form
   document.querySelector(".intervention").style.display = "block";
+  e.preventDefault()
+})
+
+//accordion to render list of red flags
+let accordion = document.getElementsByClassName("accordion");
+let i;
+
+for (i = 0; i < accordion.length; i++) {
+  accordion[i].addEventListener("click", function () {
+    //classlist is used to access the class instead of using its direct classs name
+    this.classList.toggle("active");
+
+    //this is is selecting the div
+    let panel = this.nextElementSibling;
+
+    //div is hidden in css, conditional to display or hide it
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+let editButton = document.getElementById('edit-btn')
+
+editButton.addEventListener('click', (e) => {
+  //create div and add a class
+  let div = document.createElement("div")
+  div.className = 'box'
+
+  //show form
+  document.querySelector(".edit-red-flag").style.display = "block";
   e.preventDefault()
 })
