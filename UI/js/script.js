5c76b85497ce2265e190d4fd38fe7e4e2cc214ca
vic3king <vic3coorp@gmail.com>
2018-11-20 09:10:50 +0000

    [finishes #162050282] Create an intervention request

--- UI/js/script.js
+++ UI/js/script.js
@@ -1,11 +1,29 @@
 //select dom element(button)
 let createRedTag = document.getElementById('btn1')
+let interventionRequest = document.getElementById('btn2')
 
-createRedTag.addEventListener('click', () => {
+createRedTag.addEventListener('click', (e) => {
+  //effect to make sure only one form is being displayed at a time
+  document.querySelector(".intervention").style.display = "none";
+
   //create div and add a class
   let div = document.createElement("div")
   div.className = 'box'
+
   //show form
-  document.querySelector("#red-flag").style.display = "block";
+  document.querySelector(".red-flag").style.display = "block";
+  e.preventDefault()
 })
 
+interventionRequest.addEventListener('click', (e) => {
+  //effect to make sure only one form is being displayed at a time
+  document.querySelector(".red-flag").style.display = "none";
+
+  //create div and add a class
+  let div = document.createElement("div")
+  div.className = 'box'
+  
+  //show form
+  document.querySelector(".intervention").style.display = "block";
+  e.preventDefault()
+})
