f9ba301825d1638980508129b8ff79b2e9e03ac8
vic3king <vic3coorp@gmail.com>
2018-11-22 18:14:02 +0000

    [finishes #162155799] refactor js to have reusable function, add and configure eslint

--- js/script.js
+++ js/script.js
@@ -1,107 +1,94 @@
 const accordionRemote = () => {
-    //accordion to render list of red flags
-    let accordion = document.getElementsByClassName("accordion");
+  //  accordion to render list of red flags
+  const accordion = document.getElementsByClassName('accordion');
 
-    for (let i = 0; i < accordion.length; i++) {
-        accordion[i].addEventListener("click", function () {
-            //classlist is used to access the class instead of using its direct classs name
-            this.classList.toggle("active");
+  for (let i = 0; i < accordion.length; i += 1) {
+    accordion[i].addEventListener('click', function toggle() {
+      //  classlist is used to access the class instead of using its direct classs name
+      this.classList.toggle('active');
+      //  this is is selecting the div
+      const panel = this.nextElementSibling;
+      //  div is hidden in css, conditional to display or hide it
+      if (panel.style.display === 'block') {
+        panel.style.display = 'none';
+      } else {
+        panel.style.display = 'block';
+      }
+    });
+  }
+};
 
-            //this is is selecting the div
-            let panel = this.nextElementSibling;
-            //div is hidden in css, conditional to display or hide it
-            if (panel.style.display === "block") {
-                panel.style.display = "none";
-            } else {
-                panel.style.display = "block";
-            }
-        });
-    }
-}
+// reusable conditional statement
+const conditional = (e, btnId, elClass1, elClass2, state1, state2) => {
+  // the div wont have an id of btn1, so js will check the next element which will be the button
+  if (e.target.id === btnId) {
+  // effect to make sure only one form is being displayed at a time
+    document.querySelector(elClass1).style.display = state1;
 
+    // show form
+    document.querySelector(elClass2).style.display = state2;
+    e.preventDefault();
+  }
+};
 
 function formsDisplayLogic() {
-    //parent element for the forms
-    let parentDiv1 = document.querySelector('#parent')
+  //  parent element for the forms
+  const parentDiv1 = document.querySelector('#parent');
+  const parentDiv2 = document.querySelector('#parent2');
+  function displayForm1(e) {
+    // conditional logic function
+    conditional(e, 'btn1', '.intervention', '.red-flag', 'none', 'block');
+    e.stopPropagation();
+  }
 
-    //event listeners placed on divs 
-    parentDiv1.addEventListener('click', displayForm1)
-
-    function displayForm1(e) {
-        if (e.target.id == 'btn1') {
-
-            //effect to make sure only one form is being displayed at a time
-            document.querySelector(".intervention").style.display = "none";
-
-            //show form
-            document.querySelector(".red-flag").style.display = "block";
-            e.preventDefault()
-        }
-        e.stopPropagation()
-    }
-
-    let parentDiv2 = document.querySelector('#parent2')
-    parentDiv2.addEventListener('click', displayForm2)
-
-    //callback function for eventlisteners, (n.b)arrow fuction wont use(this)
-    function displayForm2(e) {
-        //the div wont have an id of btn2, so js will check the next element which will be the button
-        if (e.target.id == 'btn2') {
-
-            //effect to make sure only one form is being displayed at a time
-            document.querySelector(".red-flag").style.display = "none";
-
-            //show form
-            document.querySelector(".intervention").style.display = "block";
-            e.preventDefault()
-        }
-
-        //stop the bubbling effect
-        e.stopPropagation()
-    }
+  // callback function for eventlisteners, (n.b)arrow fuction wont use(this)
+  function displayForm2(e) {
+    // conditional logic function
+    conditional(e, 'btn2', '.red-flag', '.intervention', 'none', 'block');
+    // stop the bubbling effect
+    e.stopPropagation();
+  }
+  // event listeners placed on divs
+  parentDiv1.addEventListener('click', displayForm1);
+  parentDiv2.addEventListener('click', displayForm2);
 }
 
 
 function editForm() {
-    let parentDiv1 = document.querySelector('#parentx')
-    let parentDiv2 = document.querySelector('#parentxy')
-    //event listener for editing a red flag button
-    parentDiv1.addEventListener('click', displayFormEdit1)
-    parentDiv2.addEventListener('click', displayFormEdit2)
+  const parentDiv1 = document.querySelector('#parentx');
+  const parentDiv2 = document.querySelector('#parentxy');
 
+  function displayFormEdit1(e) {
+    // if (e.target.id === 'btn3') {
+    //   // show form
+    //   document.querySelector('.edit-red-flag').style.display = 'block';
+    //   e.preventDefault();
+    // }
+    conditional(e, 'btn3', '.edit-red-flag', '.edit-intervention', 'block', 'none');
+    // stop the bubbling effect
+    e.stopPropagation();
+  }
 
-    function displayFormEdit1(e) {
-        if (e.target.id == 'btn3') {
-            //show form
-            document.querySelector(".edit-red-flag").style.display = "block";
-            e.preventDefault()
-        }
-        //stop the bubbling effect
-        e.stopPropagation()
-    }
-
-    function displayFormEdit2(e) {
-        if (e.target.id == 'btn4') {
-
-            //show form
-            document.querySelector(".edit-intervention").style.display = "block";
-            e.preventDefault()
-        }
-        //stop the bubbling effect
-        e.stopPropagation()
-    }
-
+  function displayFormEdit2(e) {
+    // if (e.target.id === 'btn4') {
+    //   // show form
+    //   document.querySelector('.edit-intervention').style.display = 'block';
+    //   e.preventDefault();
+    // }
+    conditional(e, 'btn4', '.edit-intervention', '.edit-red-flag', 'block', 'none');
+    // stop the bubbling effect
+    e.stopPropagation();
+  }
+  // event listener for editing a red flag button
+  parentDiv1.addEventListener('click', displayFormEdit1);
+  parentDiv2.addEventListener('click', displayFormEdit2);
 }
 
-
-
-//used to prevent error generating in admin.html
+// used to prevent error generating in admin.html
 try {
-    formsDisplayLogic()
-    editForm()
-    accordionRemote()
+  formsDisplayLogic();
+  editForm();
+  accordionRemote();
 } catch (error) {
-    accordionRemote()
+  accordionRemote();
 }
-
-
